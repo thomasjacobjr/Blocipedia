2bcb9a89d0285e23aa58204767cc85554b4a1bae
thomasjacobjr <thomasjacobjr@gmail.com>
2016-02-29 09:05:16 -0700

    Completed wikis controller actions and views

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -26,5 +26,33 @@ class WikisController < ApplicationController
   end
 
   def edit
+    @wiki = Wiki.find(params[:id])
   end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.title = params[:post][:title]
+    @wiki.body = params[:post][:body]
+
+    if @wiki.save
+      flash[:notice] = "Wiki was updated."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "There was an error saving the wiki. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @wiki = Wiki.find(params[:id])
+
+    if @wiki.destroy
+      flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+      redirect_to wikis_path
+    else
+      flash.now[:alert] = "There was an error deleting the wiki."
+      render :show
+    end
+  end
+
 end
