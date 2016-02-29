933b65b867baa90fc537d914ceb5877ce7c8be01
thomasjacobjr <thomasjacobjr@gmail.com>
2016-02-29 08:49:03 -0700

    Customized 'New Wiki' page, added new and create actions

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,12 +1,27 @@
 class WikisController < ApplicationController
   def index
-    @wikis = Wiki.all 
+    @wikis = Wiki.all
   end
 
   def show
   end
 
   def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wikie][:body]
+
+    if @wiki.save
+      flash[:notice] = "Wiki saved."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "There was an error saving the wiki. Please try again later."
+      render :new
+    end
   end
 
   def edit
