34123d18a6bdade348ec58408f29fcbd61549aad
thomasjacobjr <thomasjacobjr@gmail.com>
2016-03-21 10:08:34 -0700

    Updated wiki policy scope

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -1,11 +1,33 @@
 class WikiPolicy < ApplicationPolicy
-  class Scope < Scope
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
     def resolve
-      if user.premium? || user.admin?
-        scope.all
+      wikis = []
+      if user.role == 'admin'
+        wikis = scope.all
+      elsif user.role == 'premium'
+        all_wikis = scope.all
+        all_wikis.each do |wiki|
+          if wiki.public? || wiki.owner == user || wiki.collaborators.include?(user)
+            wikis << wiki
+          end
+        end
       else
-        scope.where(["private = ?", false])
+        all_wikis = scope.all
+        wikis = []
+        all_wikis.each do |wiki|
+          if wiki.public? || wiki.collaborators.include?(user)
+            wikis << wiki
+          end
+        end
       end
+      wikis
     end
   end
 
