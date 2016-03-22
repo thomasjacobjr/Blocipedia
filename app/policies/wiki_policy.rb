deebe081542490e08c0ef4ede52e22e03a6b620e
thomasjacobjr <thomasjacobjr@gmail.com>
2016-03-22 11:29:57 -0700

    WIP fixing wiki policy scope

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -5,13 +5,18 @@ class WikiPolicy < ApplicationPolicy
       if user.role == 'admin'
         return scope.all
       elsif user.role == 'premium'
-        all_wikis = scope.all
-        wikis = []
-        all_wikis.each do |wiki|
-          if wiki.public? || wiki.user == user || wiki.collaborations.include?(user)
-            wikis << wiki
-          end
-        end
+        tbl = scope.arel_table
+        return scope.where(tbl[:private].eq(false).or(tbl[:user_id].eq(user.id)))
+
+#        return scope.where(user: user)
+
+#        all_wikis = scope.all
+#        wikis = []
+#        all_wikis.each do |wiki|
+#          if wiki.public? || wiki.user == user || wiki.collaborations.include?(user)
+#            wikis << wiki
+#          end
+#        end
       else
         all_wikis = scope.all
         wikis = []
