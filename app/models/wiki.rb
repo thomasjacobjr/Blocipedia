67d71a116ecd0c1b9990f3d6a2a71a737b80b745
thomasjacobjr <thomasjacobjr@gmail.com>
2016-03-22 10:50:53 -0700

    Defined public method for Wiki class

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,5 +1,9 @@
 class Wiki < ActiveRecord::Base
   belongs_to :user
   has_many :collaborations
-  has_many :users, through: :collaborations 
+  has_many :users, through: :collaborations
+
+  def public?
+    self.private == false
+  end
 end
--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -2,14 +2,13 @@ class WikiPolicy < ApplicationPolicy
   class Scope < Scope
 
     def resolve
-
       if user.role == 'admin'
         return scope.all
       elsif user.role == 'premium'
         all_wikis = scope.all
         wikis = []
         all_wikis.each do |wiki|
-          if wiki.public? || wiki.owner == user || wiki.collaborators.include?(user)
+          if wiki.public? || wiki.user == user || wiki.collaborations.include?(user)
             wikis << wiki
           end
         end
